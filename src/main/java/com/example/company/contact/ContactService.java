f11089c8b101987f0b2ffa932b735491749f1597
markxkamau <markxkamau@gmail.com>
2022-07-15 12:52:32 +0300

    New function to return contacts in list format

--- src/main/java/com/example/company/contact/ContactService.java
+++ src/main/java/com/example/company/contact/ContactService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -32,4 +33,7 @@ public class ContactService {
         contactRepository.deleteById(id);
     }
 
+    public List<Contact> getContacts() {
+        return contactRepository.findAll();
+    }
 }
