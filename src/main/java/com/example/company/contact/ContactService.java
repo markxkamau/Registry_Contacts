770344ec418c15cba5462f5286a8de812ee10899
markxkamau <markxkamau@gmail.com>
2022-07-15 13:58:45 +0300

    Deleting the unused Company import

--- src/main/java/com/example/company/contact/ContactService.java
+++ src/main/java/com/example/company/contact/ContactService.java
@@ -1,6 +1,5 @@
 package com.example.company.contact;
 
-import com.example.company.company.Company;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
