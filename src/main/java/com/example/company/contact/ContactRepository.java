b0f01e2cdca0aa2e1624ab30fbc89c60308f4187
markxkamau <markxkamau@gmail.com>
2022-07-16 22:03:51 +0300

    New function to find contact object by company Id

--- src/main/java/com/example/company/contact/ContactRepository.java
+++ src/main/java/com/example/company/contact/ContactRepository.java
@@ -2,5 +2,8 @@ package com.example.company.contact;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ContactRepository extends JpaRepository<Contact,Long> {
+    Optional<Contact> findByCompanyId(Long companyId);
 }
