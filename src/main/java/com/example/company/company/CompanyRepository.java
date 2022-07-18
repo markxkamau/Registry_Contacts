16a714b1e28f590547d61f6ba04bf255cd2da8bf
markxkamau <markxkamau@gmail.com>
2022-07-18 20:34:24 +0300

    New function to get name to match against the search keyword inputted

--- src/main/java/com/example/company/company/CompanyRepository.java
+++ src/main/java/com/example/company/company/CompanyRepository.java
@@ -1,6 +1,15 @@
 package com.example.company.company;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface CompanyRepository extends JpaRepository<Company,Long> {
+import java.util.List;
+import java.util.Optional;
+
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+    @Query(value = "select * from company s where s.name like %:keyword% ", nativeQuery = true)
+    Optional<Company> findByName(@Param("keyword") String keyword);
+
+//    Optional<Company> findByName(String keyword);
 }
